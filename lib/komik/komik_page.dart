8c2a2b6df09b0542af52e977598dd6a617e04368
LAPTOP-A1444KKA\HP <004theosimorangkir@gmail.com>
2024-10-26 21:51:54 +0700

    - delete package

--- lib/komik/komik_page.dart
+++ lib/komik/komik_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:penyuluhan_narkoba_app/model/komik_data.dart';
 
 class KomikPage extends StatefulWidget {
@@ -11,13 +10,17 @@ class KomikPage extends StatefulWidget {
 class _KomikPageState extends State<KomikPage> {
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
       return Scaffold(
           appBar: AppBar(
               foregroundColor: Colors.white,
               title: const Text(
                 "Komik",
-                style: TextStyle(fontFamily: 'Avenir', fontWeight: FontWeight.w800, color: Colors.white),
+                style: TextStyle(
+                    fontFamily: 'Avenir',
+                    fontWeight: FontWeight.w800,
+                    color: Colors.white),
               ),
               centerTitle: true,
               backgroundColor: Colors.red),
